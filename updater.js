4c73f1ae508f1aa1098a09c531e5085db18d9b38
Dan Maxim <daniel.maxim@millsteel.com>
2024-03-14 11:35:16 -0400

    Add logic for handling updates

--- updater.js
+++ updater.js
@@ -1,9 +1,26 @@
+const {dialog} = require("electron");
 const {autoUpdater} = require("electron-updater");
 
 // Congfigure log debugging
 autoUpdater.logger = require("electron-log");
 autoUpdater.logger.transports.file.level = "info";
+autoUpdater.autoDownload = false;
 
 module.exports = () => {
   autoUpdater.checkForUpdates();
+
+  autoUpdater.on("update-available", () => {
+    dialog
+      .showMessageBox({
+        type: "info",
+        title: "Update available",
+        message:
+          "A new version of the app is available. Do you want to update now?",
+        buttons: ["Update", "No"],
+      })
+      .then((result) => {
+        let buttonIndex = result.response;
+        if (buttonIndex === 0) autoUpdater.downloadUpdate();
+      });
+  });
 };
